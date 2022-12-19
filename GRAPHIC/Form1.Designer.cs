4a034c62716f42078634146cb645c23fdb37045d
Ilya4124 <121015974+Ilya4124@users.noreply.github.com>
2022-12-19 12:28:40 +0200

    1

---
+++ GRAPHIC/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace GRAPHIC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buildAGraphToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cleatTheGrahpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox_h = new System.Windows.Forms.TextBox();
+            this.textBox_b = new System.Windows.Forms.TextBox();
+            this.textBox_a = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.checkBox_tan = new System.Windows.Forms.CheckBox();
+            this.checkBox_sin = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            legend1.Title = "Colors of graphs";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(6, 21);
+            this.chart.Name = "chart";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            series1.Legend = "Legend1";
+            series1.LegendText = "sin";
+            series1.Name = "Series1";
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Color = System.Drawing.Color.Navy;
+            series2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            series2.Legend = "Legend1";
+            series2.LegendText = "tangent";
+            series2.Name = "Series2";
+            this.chart.Series.Add(series1);
+            this.chart.Series.Add(series2);
+            this.chart.Size = new System.Drawing.Size(770, 440);
+            this.chart.TabIndex = 0;
+            this.chart.Text = "chart";
+            title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            title1.Name = "Title1";
+            title1.Text = "Graph of functions";
+            this.chart.Titles.Add(title1);
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.buildAGraphToolStripMenuItem,
+            this.cleatTheGrahpToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // buildAGraphToolStripMenuItem
+            // 
+            this.buildAGraphToolStripMenuItem.Name = "buildAGraphToolStripMenuItem";
+            this.buildAGraphToolStripMenuItem.Size = new System.Drawing.Size(194, 26);
+            this.buildAGraphToolStripMenuItem.Text = "Build a graph";
+            this.buildAGraphToolStripMenuItem.Click += new System.EventHandler(this.Build_a_Graph);
+            // 
+            // cleatTheGrahpToolStripMenuItem
+            // 
+            this.cleatTheGrahpToolStripMenuItem.Name = "cleatTheGrahpToolStripMenuItem";
+            this.cleatTheGrahpToolStripMenuItem.Size = new System.Drawing.Size(194, 26);
+            this.cleatTheGrahpToolStripMenuItem.Text = "Clear the grahp";
+            this.cleatTheGrahpToolStripMenuItem.Click += new System.EventHandler(this.ClearGraph);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1102, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.Close_a_Graph);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chart);
+            this.groupBox1.Location = new System.Drawing.Point(11, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(782, 467);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Craphic";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox_h);
+            this.groupBox2.Controls.Add(this.textBox_b);
+            this.groupBox2.Controls.Add(this.textBox_a);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(799, 34);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(293, 228);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Parameters";
+            // 
+            // textBox_h
+            // 
+            this.textBox_h.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_h.Location = new System.Drawing.Point(125, 148);
+            this.textBox_h.Name = "textBox_h";
+            this.textBox_h.Size = new System.Drawing.Size(145, 34);
+            this.textBox_h.TabIndex = 5;
+            // 
+            // textBox_b
+            // 
+            this.textBox_b.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_b.Location = new System.Drawing.Point(125, 87);
+            this.textBox_b.Name = "textBox_b";
+            this.textBox_b.Size = new System.Drawing.Size(145, 34);
+            this.textBox_b.TabIndex = 4;
+            // 
+            // textBox_a
+            // 
+            this.textBox_a.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_a.Location = new System.Drawing.Point(125, 31);
+            this.textBox_a.Name = "textBox_a";
+            this.textBox_a.Size = new System.Drawing.Size(145, 34);
+            this.textBox_a.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(7, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "h (step) =";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(7, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "B (to) =";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(7, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A (from) =";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.checkBox_tan);
+            this.groupBox3.Controls.Add(this.checkBox_sin);
+            this.groupBox3.Location = new System.Drawing.Point(799, 268);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(293, 227);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Graphic";
+            // 
+            // checkBox_tan
+            // 
+            this.checkBox_tan.AutoSize = true;
+            this.checkBox_tan.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox_tan.Location = new System.Drawing.Point(12, 84);
+            this.checkBox_tan.Name = "checkBox_tan";
+            this.checkBox_tan.Size = new System.Drawing.Size(114, 33);
+            this.checkBox_tan.TabIndex = 1;
+            this.checkBox_tan.Text = "tangent";
+            this.checkBox_tan.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_sin
+            // 
+            this.checkBox_sin.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox_sin.AutoSize = true;
+            this.checkBox_sin.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox_sin.Location = new System.Drawing.Point(12, 30);
+            this.checkBox_sin.Name = "checkBox_sin";
+            this.checkBox_sin.Size = new System.Drawing.Size(104, 33);
+            this.checkBox_sin.TabIndex = 0;
+            this.checkBox_sin.Text = "y = x*x";
+            this.checkBox_sin.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1102, 510);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem buildAGraphToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cleatTheGrahpToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox_a;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_h;
+        private System.Windows.Forms.TextBox textBox_b;
+        private System.Windows.Forms.CheckBox checkBox_tan;
+        private System.Windows.Forms.CheckBox checkBox_sin;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ GRAPHIC/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GRAPHIC
+{
+    public partial class Form1 : Form
+    {
+        private double a, b, h;
+        private double x, y;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    
+        public void Build_a_Graph(object sender, EventArgs e)
+        {
+            if (checkBox_sin.Checked == false && checkBox_tan.Checked == false)
+            {
+                MessageBox.Show("Choose a graphic!", "Warning!");
+                return;
+            }
+            if (textBox_a.Text == "" || textBox_b.Text == "" || textBox_h.Text == "")
+            {
+                MessageBox.Show("Default value!", "Warning!");
+                DefaultParams();
+            }
+            else
+            {
+                a = Convert.ToDouble(textBox_a.Text);
+                b = Convert.ToDouble(textBox_b.Text);
+                h = Convert.ToDouble(textBox_h.Text);
+            }
+            timer1.Enabled = true;
+
+            if (checkBox_sin.Checked)
+            {
+                x = a;
+                this.chart.Series[0].Points.Clear();
+                while (x <= b)
+                {
+                    this.chart.Update();
+                    y = Math.Pow(x,2);
+                    this.chart.Series[0].Points.AddXY(x,y);
+                    x += h;
+                }
+            }
+            if (checkBox_tan.Checked)
+            {
+                x = a;
+                this.chart.Series[1].Points.Clear();
+                while (x <= b)
+                {
+                    this.chart.Update();
+
+                    this.chart.Series[1].Points.Add(((((x * x) + (2 * x) * (x - b) - 600) *5))/1000);
+                    x += h;
+                }
+
+            }
+            timer1.Enabled = false;
+
+        }
+        public void timer1_Tick(object sender, EventArgs e)
+        {
+            Build_a_Graph(sender, e);
+        }
+
+
+        public void ClearGraph(object sender, EventArgs e)
+        {
+            if (checkBox_sin.Checked == false)
+            {
+                MessageBox.Show("Choose a graphic!", "Warning!");
+                return;
+            }
+            if (checkBox_sin.Checked)
+            {
+                this.chart.Series[0].Points.Clear();
+            }
+            if (checkBox_tan.Checked)
+            {
+                this.chart.Series[1].Points.Clear();
+            }
+        }
+
+        public void Close_a_Graph(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Exit?", "Warning!", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+        public void DefaultParams()
+        {
+            a = -10;
+            b = 10;
+            h = 0.1;
+        }
+    }
+}
